2a6ca9edc79f4312c2c225999f10dce2759b4880
Pinceris <m.kondrasovas@gmail.com>
2019-09-27 15:44:54 +0300

    video downloader remake, other updates

--- src/server/index.js
+++ src/server/index.js
@@ -11,10 +11,27 @@ app.listen(4000, () => {
     console.log('Server Works !!! At port 4000');
 });
 
+app.get('/thumb', (req,res) => {
+    res.setHeader('Content-Type', 'application/json');
+    ytdl.getInfo(req.query.URL)
+        .then((obj) => {
+            res.end(JSON.stringify({ url: obj.player_response.videoDetails.thumbnail.thumbnails[0].url }))
+        }).catch(() => {
+            res.end(JSON.stringify({ status: 'Failed to fetch url' }))
+    })
+});
+
 app.get('/download', (req,res) => {
     var URL = req.query.URL;
-    res.header('Content-Disposition', 'attachment; filename="video.mp4"');
-    ytdl(URL, {
-        format: 'mp4'
-    }).pipe(res);
+    let name = 'default';
+    ytdl.getInfo('https://www.youtube.com/watch?v=FrX3M3qJgJU')
+        .then((obj) => {
+            name = (JSON.stringify(obj.player_response.videoDetails.title))
+            res.header('Content-Disposition', 'attachment; filename=' + name + '.mp4');
+            ytdl(URL, {
+                format: 'mp4'
+            }).pipe(res)
+        }).catch(() => {
+            res.end(JSON.stringify({status: 'Failed to fetch video title'}));
+        });
 });
